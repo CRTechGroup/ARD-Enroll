667a4d5058124e2f6c0fb1f47c09d54a261b5324
Richard Ornelas <rich@terriblyrare.com>
2015-12-09 17:32:05 -0800

    Working Commit

--- ARD-enroll.sh
+++ ARD-enroll.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+#Enable ARD for Specific Users
+/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -specifiedUsers
+
 #Enable ARD Agent for CRADMIN
 /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -users cradmin -privs -all -restart -agent -menu
+
+#Enable SSH
+systemsetup -setremotelogin on
+
+#Create SSH Group
+dseditgroup -o create -q com.apple.access_ssh
+
+#Add CRADMIN to SSH Group
+dseditgroup -o edit -a cradmin -t user com.apple.access_ssh
+
